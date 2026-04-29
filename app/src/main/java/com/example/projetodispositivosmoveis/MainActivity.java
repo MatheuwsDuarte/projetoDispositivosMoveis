179090212684099840618685d4070a5fe12f2c37
Matheus Duarte <100427903+MatheuwsDuarte@users.noreply.github.com>
2026-04-29 14:53:47 -0300

    gerar numeros aleatorios

--- app/src/main/java/com/example/projetodispositivosmoveis/MainActivity.java
+++ app/src/main/java/com/example/projetodispositivosmoveis/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projetodispositivosmoveis;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        txtResultado = findViewById(R.id.txtResultado);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void sortearNumero (View view){
+
+        int x = new Random().nextInt(10) + 1;
+
+        txtResultado.setText("RESULTADO: " + x);
+
+        // Muda a cor conforme o número
+        if(x % 2 == 0) {
+            txtResultado.setTextColor(Color.CYAN);
+        } else{
+            txtResultado.setTextColor(Color.MAGENTA);
+        }
+
     }
 }
